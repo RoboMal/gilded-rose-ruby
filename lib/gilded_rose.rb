a9dc2ea1079777316ecdad481068bf43a0e46e8e
Malcolm King <malcolm.elijah.king@gmail.com>
2024-02-27 17:19:49 -0600

    Added Brie class and added brie_tick to GildedRose

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -50,7 +50,55 @@ class GildedRose
   end
 end
 
+class GildedRose
+  def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
+  end
+
+  def brie_tick
+    @item = Brie.new(quality, days_remaining)
+    item.tick
+  end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+end
+
+class Brie
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+    @days_remaining -= 1
+    return if @quality >= 50
+
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+end
+
 class Normal
+  attr_reader :quality, :days_remaining
+
   def initialize(quality, days_remaining)
     @quality, @days_remaining = quality, days_remaining
   end
