18211e6f59ad8b2bcfd118862c87458f4262bf63
Malcolm King <malcolm.elijah.king@gmail.com>
2024-02-27 22:19:25 -0600

    updated brie_tick to ensure that the quality of Brie items is incremented correctly under specified conditions

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,12 +21,12 @@ module GildedRose
   end
 
   class Brie < Item
-    def tick
+    def brie_tick
       @days_remaining -= 1
       return if @quality >= 50
 
       @quality += 1
-      @quality += 1 if @days_remaining <= 0
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
     end
   end
 
